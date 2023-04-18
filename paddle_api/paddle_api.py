bebe42d7b1da67ea127d418692e0b3073e8af3fb
Yuri <jkorbacka@gmail.com>
2023-04-18 17:51:14 +0200

    implement product create update retrieve

--- paddle_api/paddle_api.py
+++ paddle_api/paddle_api.py
@@ -1,5 +1,45 @@
 """Main module."""
+import typing
+from dataclasses import asdict, dataclass
+from enum import Enum
+
 import requests
+
+
+class Path(Enum):
+    """REST paths."""
+
+    EVENT_TYPES = "/event-types"
+    PRODUCTS = "/products"
+
+
+class BadRequest(requests.HTTPError):
+    """An Bad Request error occurred."""
+
+    def __init__(self, *args, **kwargs):
+        response = kwargs.pop("response", None)
+        error = response.json()["error"]
+        self.code = error["code"]
+        super().__init__(error["detail"], *args, **kwargs)
+
+
+@dataclass
+class Product:  # noqa: D101
+    name: str
+    tax_category: typing.Literal[
+        "standard",
+        "saas",
+        "ebooks",
+        "digital-goods",
+        "website-hosting",
+        "human-services",
+        "implementation-services",
+        "training-services",
+        "professional-services",
+        "software-programming-services",
+    ]
+    description: typing.Optional[str] = None
+    image_url: typing.Optional[str] = None
 
 
 class Paddle:
@@ -17,13 +57,44 @@ class Paddle:
             "Paddle-Version": str(api_version),
         }
 
-    def _get(self, path: str):
+    def _get_url(self, path: Path, pk: typing.Optional[str] = None):
+        url = f"{self.base_url}{path.value}"
+        if not pk:
+            return url
+        return f"{url}/{pk}"
+
+    def _get(self, path: Path, pk: typing.Optional[str] = None):
+        url = self._get_url(path, pk)
         response = requests.get(
-            url=f"{self.base_url}{path}",
+            url=url,
             headers=self.headers,
         )
         return response.json()
 
+    def _create_or_update(self, path: Path, data, pk: typing.Optional[str] = None) -> dict:
+        request_f = requests.patch if pk else requests.post
+        response = request_f(  # type: ignore
+            url=self._get_url(path, pk),
+            json=asdict(data),
+            headers=self.headers,
+        )
+        if response.status_code == 400:
+            raise BadRequest(response=response)
+        response.raise_for_status()
+        return response.json()
+
     def event_types(self):
         """Get webhook event types."""
-        return self._get("/event-types")
+        return self._get(Path.EVENT_TYPES)
+
+    def products(self, pk: typing.Optional[str] = None):
+        """Get products."""
+        return self._get(Path.PRODUCTS, pk)
+
+    def product_create(self, product: Product):
+        """Create product."""
+        return self._create_or_update(Path.PRODUCTS, data=product)
+
+    def product_update(self, product: Product, pk: str):
+        """Update (patch) product."""
+        return self._create_or_update(Path.PRODUCTS, data=product, pk=pk)
--- tests/test_paddle_api.py
+++ tests/test_paddle_api.py
@@ -3,7 +3,7 @@
 
 import pytest
 
-from paddle_api import Paddle
+from paddle_api.paddle_api import Paddle, Path
 
 FAKE_API_KEY = "fake_123"
 
@@ -32,14 +32,21 @@ def test_authorization_header():
     assert f"Bearer {FAKE_API_KEY}" == client.headers["Authorization"]
 
 
-def test__get(mocker):
+@pytest.mark.parametrize(
+    "pk,expected",
+    (
+        (None, "/event-types"),
+        ("123", "/event-types/123"),
+    ),
+)
+def test__get(pk, expected, mocker):
     # arrange
     mock_get = mocker.patch("requests.get")
     client = Paddle(FAKE_API_KEY)
 
     # act
-    client._get("/fake-path")
+    client._get(Path.EVENT_TYPES, pk)
 
     # assert
     mock_get.assert_called_once()
-    assert "/fake-path" in mock_get.call_args_list[0].kwargs["url"]
+    assert expected in mock_get.call_args_list[0].kwargs["url"]
